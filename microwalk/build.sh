1852db1b861c6c916ac5da826fc05068b699454d
Amos Treiber <amos.treiber@rohde-schwarz.com>
2024-03-06 11:29:05 +0100

    undo; debug info

--- microwalk/build.sh
+++ microwalk/build.sh
@@ -25,6 +25,9 @@ do
   targetName=$(basename -- ${target%.*})
   
   # TODO Adjust command line to link against your library
+  ls $mainDir
+  ls $mainDir/build
+
   gcc -O0 -g main.c $targetName.c -fno-inline -fno-split-stack -L "$mainDir" -lbotan-3 -I "$mainDir/build/include" -o $targetName
   
   pushd $MAP_GENERATOR_PATH
